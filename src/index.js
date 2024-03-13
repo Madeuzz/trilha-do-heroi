c639031aef61f27c506e806fe69fdbb6849b8fa7
Madeuzz <matheuscosta3107@hotmail.com>
2024-03-13 01:23:36 -0300

    0.3

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+
+//Classe de Herói
 class hero{
     constructor(name,age,job){
         this.name = name
@@ -5,6 +7,7 @@ class hero{
         this.job = job.charAt(0).toUpperCase() + job.slice(1).toLowerCase()
     }
 
+//Função para ataque do herói
     atack(){
 
         let power = ""
@@ -21,11 +24,7 @@ class hero{
             case "Ninja":
                 power = "Shurikens"
                 break
-            default:
-                console.log("Por favor escolha sua classe:")
-                break
         }
-
         console.log(`O ${this.job} atacou usando ${power}`)
     }
 }
